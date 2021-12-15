07b13aef89bf845d4ff06368334ce1a3ffde1588
youliso <youliso@hotmail.com>
2021-12-15 17:26:52 +0800

    feat: update

--- src/common/net.ts
+++ src/common/net.ts
@@ -129,7 +129,7 @@ export function upload(url: string, params: RequestUploadOpt) {
  * @param sendData
  * @param params
  */
-export function download(url: string, params: RequestDownloadOpt = {}) {
+function download(url: string, params: RequestDownloadOpt = {}) {
   return new Promise((resolve, reject) => {
     params.method = params.method || 'GET';
     params.args = params.args || { method: params.method };
