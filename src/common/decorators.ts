97896b36a4f508cc146a7f9f941f76196923c26d
youliso <youliso@hotmail.com>
2022-01-01 14:53:21 +0800

    fix: decorators

--- src/common/decorators.ts
+++ src/common/decorators.ts
@@ -20,15 +20,14 @@ export interface Route extends RouteShorthandOptions {
  */
 export const routes: Route[] = [];
 
-
 /**
  * 给controller添加装饰
  * @param {*} path
  */
 export function Controller(path?: string) {
   return function (target: any) {
-    if (!path) target.prefix = `/${target.name}`
-    else if (path.startsWith('/')) target.prefix = path
+    if (!path) target.prefix = '';
+    else if (path.startsWith('/')) target.prefix = path;
     else target.prefix = `/${path}`;
   };
 }
@@ -38,10 +37,9 @@ export function Controller(path?: string) {
  */
 function CoreMapping(route: Route = {}) {
   return function (target: any, name: string) {
-    if (!route.url) route.url = `/${name}`
-    else if (route.url.startsWith('/')) route.url = route.url
+    if (!route.url) route.url = `/${name}`;
+    else if (route.url.startsWith('/')) route.url = route.url;
     else route.url = `/${route.url}`;
-    route.method = route.method;
     route.preHandler = route.preHandler || [];
     route.handler = target[name];
     route.constructor = target.constructor;
@@ -53,34 +51,6 @@ function CoreMapping(route: Route = {}) {
  * 给controller类的方法添加装饰
  * @param params Controllers
  */
-export function RequestMapping(route: Route = {}) {
-  return CoreMapping({ method: 'GET', ...route })
-}
-
-export function GetMapping(url?: string) {
-  return CoreMapping({ method: 'GET', url: url})
-}
-
-export function DeleteMapping(url?: string) {
-  return CoreMapping({ method: 'DELETE', url: url })
-}
-
-export function HeadMapping(url?: string) {
-  return CoreMapping({ method: 'HEAD', url: url })
-}
-
-export function PatchMapping(url?: string) {
-  return CoreMapping({ method: 'PATCH', url: url })
-}
-
-export function PostMapping(url?: string) {
-  return CoreMapping({ method: 'POST', url: url })
-}
-
-export function PutMapping(url?: string) {
-  return CoreMapping({ method: 'PUT', url: url })
-}
-
-export function OptionsMapping(url?: string) {
-  return CoreMapping({ method: 'OPTIONS', url: url })
+export function RequestMapping(route: Route = { method: 'GET' }) {
+  return CoreMapping(route);
 }
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,15 +1,13 @@
 import type { FastifyRequest, FastifyReply } from 'fastify';
-import { Controller, RequestMapping, GetMapping, PostMapping } from '@/common/decorators';
+import { Controller, RequestMapping } from '@/common/decorators';
 import { Success } from '@/common/restful';
 import IndexServer from '@/servers';
 
 const indexServer = new IndexServer();
 
-@Controller('')
+@Controller()
 export class Index {
   @RequestMapping()
-  @GetMapping('test')
-  @PostMapping('test')
   async index(request: FastifyRequest, reply: FastifyReply) {
     indexServer.test();
     reply.send(Success('ok'));
