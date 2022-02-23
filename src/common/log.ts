e8c0e7f53a8d3c41acf36e8a16328d363382859b
youliso <youliso@hotmail.com>
2022-02-23 16:03:17 +0800

    fix: del e

--- src/common/log.ts
+++ src/common/log.ts
@@ -1,7 +1,6 @@
 import { appendFile } from 'fs';
 import { resolve, sep } from 'path';
 import { access, mkdir } from '@/common/file';
-import { dateFormat } from '@/utils';
 
 /**
  * 日志处理
@@ -12,6 +11,11 @@ export async function loggerInit() {
   if (!isAccess) await mkdir(logsPath);
 }
 export async function loggerWrite(msg: string) {
-  const path = logsPath + `${sep}${dateFormat('yyyy-MM-dd')}.log`;
-  appendFile(path, `[${dateFormat('yy-MM-dd hh:mm:ss')}] ${msg}`, () => {});
+  const date = new Date();
+  const path = logsPath + `${sep}${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}.log`;
+  appendFile(
+    path,
+    `[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] ${msg}`,
+    () => {}
+  );
 }
